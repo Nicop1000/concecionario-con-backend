69d379c96c4a287d572002fa6f69db152d7b942d
Nicop100LL <nicolasbrusaferri@gmail.com>
2023-07-08 11:54:11 -0300

    Seagregó static

--- concecionario/settings.py
+++ concecionario/settings.py
@@ -169,6 +169,9 @@ if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
