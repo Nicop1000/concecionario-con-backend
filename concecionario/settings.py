dbd9254b877be24f666f4429a35c8d56c935cd07
Nicop100LL <nicolasbrusaferri@gmail.com>
2023-07-12 11:51:36 -0300

    ww

--- concecionario/settings.py
+++ concecionario/settings.py
@@ -150,7 +150,8 @@ if not DEBUG:
 
     # s3 static settings
     STATIC_LOCATION = 'static'
-    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
+    #STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
+    STATIC_URL = '/static/'
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     
     
