91d447d0ef54e89dcd398737c021fe3dbea41d55
Nicop100LL <nicolasbrusaferri@gmail.com>
2023-07-12 19:43:45 -0300

    aa

--- concecionario/settings.py
+++ concecionario/settings.py
@@ -182,7 +182,7 @@ if not DEBUG:
     AWS_DEFAULT_ACL = None
     STATICFILES_LOCATION = 'static'
     MEDIAFILES_LOCATION = 'media'
-    STATICFILES_STORAGE = 'concecionario.backends.s3boto3.S3Boto3Storage'
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
     STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
     MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
 
